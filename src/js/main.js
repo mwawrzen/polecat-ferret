e72c5284f14f721060ba3289e6abdfcbef4cba83
mwawrzen <w.w@op.pl>
2021-06-03 01:12:56 +0200

    Revert "Add contact page rwd"
    This reverts commit 7f52e47d496a8dcf44acf417ab3ad5545908fbbd.

--- src/js/main.js
+++ src/js/main.js
@@ -25,13 +25,3 @@ const contactOverlay = document.querySelector('.contact-overlay');
 const contactOverlayButton = document.querySelector('.contact-overlay__button');
 
 const navlineOptionButtons = document.querySelectorAll('.page__nl-circle');
-
-/* rwd breakpoints */
-
-const breakpoints = {
-    xs: 512,
-    sm: 768,
-    md: 896,
-    lg: 1152,
-    xl: 1280
-}

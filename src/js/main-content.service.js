1879b5571707afa8592fe242958633678457c5d9
mwawrzen <w.w@op.pl>
2021-06-03 13:15:47 +0200

    Add content rwd

--- src/js/main-content.service.js
+++ src/js/main-content.service.js
@@ -10,16 +10,15 @@ const readMoreFunction = () => {
         pageContents[c].scrollTop = 0;
         document.body.style.overflow = 'scroll';
 
+        pageDescription[c].style.display = 'none';
         anim(pageContents[c], { top: '20vh' });
-        anim(pageContainer[c], { top: '150px' });
+        anim(pageContainer[c], { top: '20vh' });
         if (window.innerWidth <= breakpoints.xs) {
-            console.log('mobile');
-            anim(pageHeader[c], { fontSize: '20px' });
+            anim(pageHeader[c], { fontSize: '2vh' });
         } else {
-            console.log('desktop');
-            anim(pageHeader[c], { fontSize: '40px' });
+            anim(pageHeader[c], { fontSize: '4vh' });
         }
-        anim(pageDescription[c], { opacity: 0 });
+        anim(pageDescription[c], { opacity: 0, display: 'none' });
         anim(pageReadMoreButton[c], { opacity: 0 });
         anim(navMenu, { opacity: 0 });
 
@@ -37,6 +36,7 @@ const readMoreFunction = () => {
 
         document.body.style.overflow = 'hidden';
 
+        pageDescription[c].style.display = 'block';
         anim(pageContents[c], { top: '100vh' });
         anim(pageContainer[c], { top: '50%' });
         if (window.innerWidth <= breakpoints.xs) {

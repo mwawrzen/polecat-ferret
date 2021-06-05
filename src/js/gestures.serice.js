f2a20fa85b3d564efe2a12d71add1ad6af8de83b
mwawrzen <w.w@op.pl>
2021-06-05 11:31:46 +0200

    Add main navigation gestures

---
+++ src/js/gestures.serice.js
@@ -0,0 +1,19 @@
+let position = 0;
+
+window.addEventListener('touchstart', e => {
+
+    position = e.touches[0].screenX;
+});
+
+window.addEventListener('touchend', e => {
+
+    const endPosition = e.changedTouches[0].screenX;
+    if (endPosition > position) {
+        if (c <= pagesNumber && c > 0)
+            c--;
+    } else if (endPosition < position) {
+        if (c < pagesNumber)
+            c++;
+    }
+    navTo(c);
+});
